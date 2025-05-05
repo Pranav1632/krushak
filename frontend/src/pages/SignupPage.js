188653751c355ba8530a2167009e4ebc4837f6c5
Pranav Gaikwad <pranavgaikwad1632@gmail.com>
2025-05-06 00:27:25 +0530

    Update SignupPage.js

--- frontend/src/pages/SignupPage.js
+++ frontend/src/pages/SignupPage.js
@@ -28,7 +28,7 @@ const SignupPage = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:8000/api/auth/signup", {
+      const response = await fetch("https://krushak-backend.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
