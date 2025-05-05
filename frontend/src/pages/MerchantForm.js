9e5a82ba9c805431fc3c6deb3f43d59a6afed6ad
Pranav Gaikwad <pranavgaikwad1632@gmail.com>
2025-05-06 00:26:19 +0530

    Update MerchantForm.js

--- frontend/src/pages/MerchantForm.js
+++ frontend/src/pages/MerchantForm.js
@@ -43,7 +43,7 @@ const MerchantForm = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:8000/api/forms/merchant", {
+      const response = await fetch("https://krushak-backend.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
